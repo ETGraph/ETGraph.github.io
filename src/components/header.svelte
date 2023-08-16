f1ca859537f7e501eb17e7efd85d4724fcd59d66
lirc572 <lirc572@gmail.com>
2023-08-16 22:39:28 +0800

    update icon

--- src/components/header.svelte
+++ src/components/header.svelte
@@ -21,6 +21,7 @@
             class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4"
         >
             <a href={links.home} class="flex items-center">
+                <img src="/favicon.png" class="h-8 mr-3" alt="ETGraph Logo">
                 <span
                     class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white"
                     >ETGraph</span
