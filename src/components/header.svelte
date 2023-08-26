60710d7e2178aa800dba2b45bdc0668da7755a99
lirc572 <lirc572@gmail.com>
2023-08-27 02:26:42 +0800

    update header collapse to use js api

--- src/components/header.svelte
+++ src/components/header.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
     import { onMount } from "svelte";
-    import { Dropdown } from "flowbite";
+    import { Collapse, Dropdown } from "flowbite";
     import type { DropdownOptions } from "flowbite";
 
     export let title = "ETGraph";
@@ -17,11 +17,9 @@
         github: import.meta.env.PUBLIC_GITHUB_URL,
     };
 
-    let dropdown: Dropdown;
-
     onMount(() => {
-        const dropDownTargetEl = document.getElementById("dropdownNavbar");
-        const dropDownTriggerEl = document.getElementById("dropdownNavbarLink");
+        const dropDownTargetEl = document.getElementById("dropdownEl");
+        const dropDownTriggerEl = document.getElementById("dropdownTrigger");
         const dropDownOptions: DropdownOptions = {
             placement: "bottom",
             triggerType: "click",
@@ -30,16 +28,16 @@
             delay: 300,
             ignoreClickOutsideClass: false,
         };
-        dropdown = new Dropdown(
-            dropDownTargetEl,
-            dropDownTriggerEl,
-            dropDownOptions
-        );
+        new Dropdown(dropDownTargetEl, dropDownTriggerEl, dropDownOptions);
+
+        const collapseTargetEl = document.getElementById("collapseEl");
+        const collapseTriggerEl = document.getElementById("collapseTrigger");
+        const collapseOptions = {};
+        new Collapse(collapseTargetEl, collapseTriggerEl, collapseOptions);
     });
 </script>
 
 <header
-    id="header"
     class="sticky top-0 z-40 flex-none w-full mx-auto dark:border-b dark:border-gray-200 dark:bg-gray-800"
 >
     <nav>
@@ -54,10 +52,9 @@
                 >
             </a>
             <button
-                data-collapse-toggle="navbar-dropdown"
                 type="button"
+                id="collapseTrigger"
                 class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-                aria-controls="navbar-dropdown"
                 aria-expanded="false"
             >
                 <span class="sr-only">Open main menu</span>
@@ -77,7 +74,7 @@
                     />
                 </svg>
             </button>
-            <div class="hidden w-full md:block md:w-auto" id="navbar-dropdown">
+            <div class="hidden w-full md:block md:w-auto" id="collapseEl">
                 <ul
                     class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700"
                 >
@@ -90,7 +87,7 @@
                     </li>
                     <li>
                         <button
-                            id="dropdownNavbarLink"
+                            id="dropdownTrigger"
                             class="flex items-center justify-between w-full py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-white md:dark:hover:text-blue-500 dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent"
                             >Tasks <svg
                                 class="w-2.5 h-2.5 ml-2.5"
@@ -110,7 +107,7 @@
                         >
                         <!-- Dropdown menu -->
                         <div
-                            id="dropdownNavbar"
+                            id="dropdownEl"
                             class="z-10 hidden font-normal divide-y divide-gray-100 rounded-lg shadow w-44 bg-gray-50 dark:bg-gray-700 dark:divide-gray-600"
                         >
                             <ul
@@ -195,7 +192,7 @@
 </header>
 
 <style>
-    #header {
-        background-color: #C1C19C;
+    header {
+        background-color: #c1c19c;
     }
 </style>
